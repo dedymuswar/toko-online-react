60723cfa4d553f520d720d31f34b475193696d6f
dedymuswar <dedymuswar@gmail.com>
2021-07-04 16:30:04 +0900

    update menu-items untuk navigasi atau berpindah halaman withRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,29 +10,34 @@ export class Directory extends Component {
                 {
                     title: 'topi',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1
+                    id: 1,
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'baju',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2
+                    id: 2,
+                    linkUrl: ''
                 },
                 {
                     title: 'sepatu',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3
+                    id: 3,
+                    linkUrl: ''
                 },
                 {
                     title: 'wanita',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl: ''
                 },
                 {
                     title: 'pria',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl: ''
                 }
             ]
         }
@@ -41,8 +46,8 @@ export class Directory extends Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({id,title, imageUrl}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} />
+                {this.state.sections.map(({id,title, imageUrl, linkUrl}) => (
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} linkUrl={linkUrl} />
                 ))}
             </div>
         )
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+
 import './menu-item.style.scss'
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+    // console.log({history, match}); match.url adalah '/'
     return (
-        <div
+        <div onClick={() => history.push(`${match.url}${linkUrl}`) }
             className={`${size} menu-item`}>
             <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
             <div className="content">
@@ -13,4 +16,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
     )
 }
 
-export default MenuItem
+export default withRouter(MenuItem)
