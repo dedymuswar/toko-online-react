ef701f61e20a128c0b112f52609f4c61da717dfd
dedymuswar <dedymuswar@gmail.com>
2021-07-04 18:16:09 +0900

    update halaman shoppage, collection-preview dan collection-item component

--- src/App.js
+++ src/App.js
@@ -3,41 +3,14 @@ import './App.css';
 import { Route, Switch, Link } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
-
-const HalamanTopi = (props) => {
-    console.log(props);
-    return (
-        <div>
-            <Link to="topics">topics</Link>
-            <button onClick={() => props.history.push('/topics')}>topi</button>
-            <h1>Halaman Topi</h1>
-        </div>
-    )
-}
-const TopicList = (props) => {
-    console.log(props)
-    return (
-        <div>Topic List</div>
-    )
-}
-
-
-const TopicDetail = (props) => {
-    console.log(props)
-    return (
-        <div>Topic Detail {props.match.params.topicId}</div>
-    )
-}
-
+import ShopPage from './pages/shoppage/shoppage.component'
 
 function App() {
     return (
         <div>
             <Switch>
                 <Route exact path="/" component={HomePage}></Route>
-                <Route exact path="/hats" component={HalamanTopi}></Route>
-                <Route exact path="/topics" component={TopicList}></Route>
-                <Route exact path="/topics/:topicId" component={TopicDetail}></Route>
+                <Route exact path="/shop" component={ShopPage}></Route>
             </Switch>
         </div>
     )
